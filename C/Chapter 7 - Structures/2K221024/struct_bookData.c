fc5c183f08c46ca46469bddba2eff2221043961b
SurajL <salandage@gmail.com>
2022-10-25 22:10:35 +0530

    2K221025

--- C/Chapter 7 - Structures/2K221024/struct_bookData.c
+++ C/Chapter 7 - Structures/2K221024/struct_bookData.c
@@ -16,8 +16,9 @@ struct bookData{
 };
 
 void operations(struct bookData bData[count]);
-void displayAll(struct bookData bData[count]);
-void displaySearchData(struct bookData bData[count]);
+void displayAll(struct bookData bData[count],int);
+int displaySearchData(struct bookData bData[count]);
+void compairePrice(struct bookData bData[count]);
 
 int main(){
 
@@ -68,7 +69,7 @@ void operations(struct bookData bData[count]){
             break;
 
             case 2:
-
+                compairePrice(bData);
             break;
 
             case 3:
@@ -84,10 +85,12 @@ void operations(struct bookData bData[count]){
 }
 
 
-void displayAll(struct bookData bData[count]){
+void displayAll(struct bookData bData[count], int ch){
+
+    int a = displaySearchData(bData);
 
     for(int i=0; i<count; i++){
-        if(ch > 0 && ch < 5){
+        if(a > 0 && a < 5){
             printf("\n%d | %s | %s | %.2f INR",bData[i].id,bData[i].name,bData[i].author,bData[i].price);
         }
         else if(ch == 5){
@@ -141,7 +144,7 @@ int displaySearchData(struct bookData bData[count]){
 
         case 5:
             printf("\nDisplaying All Books Data...\n");
-            displayAll(bData);
+            displayAll(bData,ch);
         break;
 
         case 6:
@@ -164,5 +167,89 @@ int displaySearchData(struct bookData bData[count]){
 
     return ch;
 
+}
+
+void compairePrice(struct bookData bData[count]){
+    int chs;
+    float pr;
+    int i, prf,prt,tmp;
+
+    do{
+        printf("\n:::::::OPTIONS::::::: \n \
+               1. Search Book's by price \n \
+               2. Search book price less than \n \
+               3. Search book price greater than \n \
+               4. Search condition \n \
+               5. Exit... \n \
+               ");
+
+        printf("\nYour choice: ");
+        scanf("%d",&chs);
+
+        switch(chs){
+        case 1:
+                printf("\nEnter price: ");
+                scanf("%f",&pr);
+                for(int i=0; i<count; i++){
+                    printf("\n%d | %s | %s | %.2f INR",bData[i].id,bData[i].name,bData[i].author,bData[i].price);
+                }
+        break;
+
+        case 2:
+                printf("\nEnter price: ");
+                scanf("%f",&pr);
+                for(int i=0; i<count; i++){
+                    if(pr >= bData[i].price){
+                        printf("\n%d | %s | %s | %.2f INR",bData[i].id,bData[i].name,bData[i].author,bData[i].price);
+                    }
+                    else{
+                        printf("\nInvalid Data :(\n");
+                    }
+                }
+        break;
+
+        case 3:
+                printf("\nEnter price: ");
+                scanf("%f",&pr);
+                for(int i=0; i<count; i++){
+                    if(pr <= bData[i].price){
+                        printf("\n%d | %s | %s | %.2f INR",bData[i].id,bData[i].name,bData[i].author,bData[i].price);
+                    }
+                    else{
+                        printf("\nInvalid Data :(\n");
+                    }
+                }
+        break;
+
+        case 4:
+
+            printf("\nEnter from price: ");
+            scanf("%d",&prf);
+
+            printf("\nEnter to price: ");
+            scanf("%d",&prt);
+
+            for(i=0; i<count; i++){
+            if(prf <= bData[i].price && prt >= bData[i].price){
+                tmp = i;
+            }
+
+            if(tmp != -1){
+                printf("\n%d | %s | %s | %.2f INR",bData[tmp].id,bData[tmp].name,bData[tmp].author,bData[tmp].price);
+            }
+            else{
+                printf("\nInvalid Data :(\n");
+            }
+            }
+
+
+        break;
+
+        default:
+            printf("\nInvalid Data :(\n");
+        break;
+        }
+
+    }while(chs != 4);
 }
 
