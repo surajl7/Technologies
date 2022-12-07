405b453611865bd3be6039af94c216cd9c79d005
SurajL <salandage@gmail.com>
2022-12-07 22:35:17 +0530

    2K221207

---
+++ Python/4. Controle Statements - Looping - for, while, nesting loops - Break, Continue, Pass/2K221207/loops_nestedWhile.py
@@ -0,0 +1,10 @@
+# Loops - Nested While Loop
+
+i = 1
+while i <= 5:
+    print("Hi\n", end="")
+    j = 1
+    while j <= 4:
+        print("Hello\n", end="")
+        j = j + 1
+    i = i + 1
---
+++ Python/4. Controle Statements - Looping - for, while, nesting loops - Break, Continue, Pass/2K221207/loops_while.py
@@ -0,0 +1,5 @@
+# While Loop
+i = 1
+while i <= 5:
+    print("Hi")
+    i = i + 1
