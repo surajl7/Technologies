aaf3032803811f9d8ed1f34431a98e276d569932
SurajL <salandage@gmail.com>
2023-02-06 20:30:35 +0530

    2K230206

--- Back End/C++/Chapter 7 - Inheritance - Extending Classes/2K230204/inheritance_hierarchicalInheritance.cpp
+++ Back End/C++/Chapter 7 - Inheritance - Extending Classes/2K230204/inheritance_hierarchicalInheritance.cpp
@@ -16,8 +16,8 @@ Example:
                         ------------
                         | Students |
                         ------------
-                        |     |     |
-            -------------     |     --------------                                         
+                        |     |    |
+            -------------     |    ---------------                                         
             |                 |                  |
         --------       ---------------      -----------
         | Arts |       | Engineering |      | Medical |
@@ -31,3 +31,97 @@ Example:
 
     Fig. Hierachical classification of students
 */
+
+#include<iostream>
+
+class engineering;
+class arts;
+class medical;
+class mechanical;
+class electronics;
+class civil;
+
+
+//BASE CLASS---------------------------------------------------------------
+class students
+{
+    private:
+        int roll_number;
+        char sname[20];
+    public:
+        void get_data(int, char []);
+        void display(void);
+};      
+
+void students::get_data(int rolnum, char name[20])
+{
+    roll_number = rolnum;
+
+    for(int i = 0; name[20] != '\0', i++)
+    {
+        sname[i] = name[i];
+        sname[i] = '\0';
+    }
+    // sname[20] = *name;
+}
+
+void students::display(void)
+{
+    std::cout << "Roll No: " << roll_number << "\t|\tName: " << sname;
+}
+
+//------------------------------------------------------------------------
+
+class engineering : public students
+{
+    private:
+        float math_marks;
+    public:
+        void get_marks(float);
+};
+
+void engineering::get_marks(float marks)
+{
+    math_marks = marks;
+}
+
+//------------------------------------------------------------------------
+
+class mechnical : public engineering
+{
+    private:
+        float mech_sb1;
+        float mech_sb2;
+        float mech_sb3;
+    public:
+        void get_marks(float, float, float);
+        void put_marks(void);
+};
+
+void mechnical::get_marks(float sb1, float sb2, float sb3)
+{   
+    mech_sb1 = sb1;
+    mech_sb2 = sb2;
+    mech_sb3 = sb3;
+}
+
+void mechnical::put_marks(void)
+{
+    std::cout << "Mech Subject 1 Marks: " << mech_sb1 << "\n"
+              << "Mech Subject 2 Marks: " << mech_sb2 << "\n"
+              << "Mech Subject 3 Marks: " << mech_sb3 << "\n";
+}
+
+//------------------------------------------------------------------------
+
+int main()
+{
+    mechnical s1;
+
+    s1.get_data(1, "abc");
+
+    s1.get_marks(99.0, 83.20, 78.90);
+
+    s1.display();
+    s1.put_marks();
+}
