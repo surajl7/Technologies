4084f28d5883a7b5f2ee11820ebd528b6cc5b567
SurajL <salandage@gmail.com>
2023-03-21 22:42:26 +0530

    2K230321

--- Back End/Python/Chapter 09 - Object-oriented programming (OOPs)/Concept 03 - Inheritance/2K230320/inheritance_classificationOfBankAccounts.py
+++ Back End/Python/Chapter 09 - Object-oriented programming (OOPs)/Concept 03 - Inheritance/2K230320/inheritance_classificationOfBankAccounts.py
@@ -7,25 +7,16 @@
 class accounts:
     def __init__(self):
         self.account_number = None
-        self.account_type = None
         self.balance = None
         self.interest_rate = None
         self.total_returns = None
 
     def account_number_fun(self, acc_num):
+        # self.account_number = int(input("Enter account number: "))
         self.account_number = acc_num
 
-    def account_type_fuc(self, ch):
-        if ch == "s" or ch == "S":
-            self.account_type == "Savings"
-        elif ch == "c" or ch == "C":
-            self.account_type == "Current"
-        elif ch == "fd" or ch == "FD":
-            self.account_type == "Fixed deposit"
-        else:
-            print("Invalid input :(")
-
     def balance_fun(self, bal):
+        # self.balance = int(input("Enter amount to be deposited: "))
         self.balance = bal
 
     def returns_fun(self, interest_rate):
@@ -33,95 +24,80 @@ class accounts:
         self.total_returns = (self.interest_rate * self.balance) + self.balance
         return self.total_returns
 
-    def display(self):
+    def display_acc(self):
         print("Account number | Account type | Balance | Total returns", end="\n")
-        # print(self.account_number, "|", self.account_type, "|", self.balance, end="\n")
 
 
 class savings(accounts):
-    # def __init__(self):
-    #     self.interest_rate = 0.04
-    #     self.total_returns = None
+    def __init__(self):
+        self.account_type = "Savings"
 
-    def display(self):
+    def display_savings(self):
         accounts.returns_fun(self, 0.04)
+        accounts.display_acc(self)
         print(self.account_number, "|", self.account_type, "|", self.balance, "|", self.total_returns, end="\n")
 
 
 class current(accounts):
-    # def __init__(self):
-    #     self.interest_rate = 0
-    #     self.total_returns = None
+    def __init__(self):
+        self.account_type = "Current"
 
-    def display(self):
+    def display_current(self):
         accounts.returns_fun(self, 0)
         print(self.account_number, "|", self.account_type, "|", self.balance, "|", self.total_returns, end="\n")
 
 
-class fixed_deposite_fun(accounts):
-    # def __init__(self):
-    #     self.interest_rate = 0.06
-    #     self.term_type = None
+class fixed_deposit_fun(accounts):
+    def __init__(self):
+        self.account_type = "Fixed Deposit"
+        self.total_returns = None
 
-    def term_type_fun(self, ch):
-        if ch == "s" or ch == "S":
-            self.term_type = "Short Term"
-        elif ch == "m" or ch == "M":
-            self.term_type = "Medium Term"
-        elif ch == "l" or ch == "L":
-            self.term_type = "Long Term"
-        else:
-            print("Invalid Input :(")
-
-    def fd_return_fun(self):
+    def fd_return_fun(self, interest_rate):
+        self.interest_rate = interest_rate
         self.total_returns = (self.interest_rate * self.total_returns) + self.balance
         return self.total_returns
 
-    def display(self):
+    def display_fd(self):
         print("Account number | FD Investment term | Balance | Total returns", end="\n")
 
 
-class fd_short_fun(fixed_deposite_fun):
-    # def __init__(self):
-    #     self.interest_rate = 0.06
-    #     self.total_returns = None
+class fd_short_fun(fixed_deposit_fun):
+    def __init__(self):
+        self.term_type = "Short term Fixed Deposit"
 
-    def display(self):
-        fixed_deposite_fun.fd_returns_fun(self, 0.06)
+    def display_fd_s(self):
+        fixed_deposit_fun.fd_return_fun(self, 0.06)
+        fixed_deposit_fun.display_fd(self)
         print(self.account_number, "|", self.term_type, "|", self.balance, "|", self.total_returns, end="\n")
 
 
-class fd_medium_fun(fixed_deposite_fun):
-    # def __init__(self):
-    #     self.interest_rate = 0.07
-    #     self.total_returns = None
+class fd_medium_fun(fixed_deposit_fun):
+    def __init__(self):
+        self.term_type = "Medium term Fixed Deposit"
 
-    # def return_fun(self):
-    #     self.total_returns = (self.interest_rate * self.total_returns) + self.balance
-    #     return self.total_returns
-
-
-    def display(self):
-        fixed_deposite_fun.fd_returns_fun(self, 0.07)
+    def display_fd_m(self):
+        fixed_deposit_fun.fd_return_fun(self, 0.07)
+        fixed_deposit_fun.display_fd(self)
         print(self.account_number, "|", self.term_type, "|", self.balance, "|", self.total_returns, end="\n")
 
 
-class fd_long_fun(fixed_deposite_fun):
-    # def __init__(self):
-    #     self.interest_rate = 0.09
-    #     self.total_returns = None
-    #
-    # def return_fun(self):
-    #     self.total_returns = (self.interest_rate * self.total_returns) + self.balance
-    #     return self.total_returns
+class fd_long_fun(fixed_deposit_fun):
+    def __init__(self):
+        self.term_type = "Long term Fixed Deposit"
 
-    def display(self):
-        fixed_deposite_fun.fd_returns_fun(self, 0.09)
+    def display_fd_l(self):
+        fixed_deposit_fun.fd_return_fun(self, 0.09)
+        fixed_deposit_fun.display_fd(self)
         print(self.account_number, "|", self.term_type, "|", self.balance, "|", self.total_returns, end="\n")
 
 
 acc1 = savings()
-acc1.account_number_fun(11111)
-acc1.account_type_fuc("s")
-acc1.display()
+acc1.account_number_fun(1111)
+acc1.balance_fun(100)
+acc1.display_savings()
 
+
+acc2 = fd_short_fun()
+acc2.account_number_fun(2222)
+acc2.balance_fun(100)
+acc2.display_fd_s()
